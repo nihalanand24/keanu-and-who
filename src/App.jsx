5a2a65f3e74639863d5670d52dcaa47ddf6a2d9c
nihalanand <nihalanand@gmail.com>
2021-03-09 09:03:44 -0500

    changed movie title styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,21 @@ import './styles/App.scss';
 import Header from './Header';
 import SearchBar from './SearchBar';
 import { useState } from 'react';
+// import Movies from './Movies';
 import getCommonMovies from './getCommonMovies';
 import MoviesGallery from './MoviesGallery';
 
 function App() {
   const [moviesArray, setMoviesArray] = useState([]);
-  const [actor1, setActor1] = useState('');
-  const [actor2, setActor2] = useState('');
-
-  // const [submitted, setSubmitted] = useState(false);
+  const [search1, setSearch1] = useState('');
+  const [search2, setSearch2] = useState('');
 
   const handleSubmit = (event) => {
     event.preventDefault();
     
-    getCommonMovies(actor1, actor2).then((response) => {
+    console.log(search1, search2);
+
+    getCommonMovies(search1, search2).then((response) => {
       setMoviesArray(response);
       console.log(moviesArray);
     });
@@ -27,14 +28,14 @@ function App() {
     <>
       <Header />
       <SearchBar
-        value1={actor1}
-        value2={actor2}
+        value1={search1}
+        value2={search2}
         onSubmit={handleSubmit}
         onChange1={(event) => {
-          setActor1(event.target.value);
+          setSearch1(event.target.value);
         }}
         onChange2={(event) => {
-          setActor2(event.target.value);
+          setSearch2(event.target.value);
         }}
         placeholder1='Keanu'
         placeholder2='Halle Berry'
---
+++ src/Movies.jsx
@@ -0,0 +1,133 @@
+import axios from 'axios';
+
+const baseUrl = 'https://api.themoviedb.org/3';
+const apiKey = '0f71218e40b140c550833011fa9c4afb';
+
+const Movies = ({ actor1, actor2 }) => {
+  const getActorIDs = async (actor1Name, actor2Name) => {
+    const getAxiosConfig = (actor) => {
+      return {
+        method: 'GET',
+        url: `${baseUrl}/search/person`,
+        dataResponse: 'JSON',
+        params: {
+          api_key: apiKey,
+          query: actor,
+        },
+      };
+    };
+
+    const actor1Config = getAxiosConfig(actor1Name);
+
+    const actor2Config = getAxiosConfig(actor2Name);
+
+    const actor1ID = await axios(actor1Config)
+      .then((res) => res.data.results[0])
+      .then((person) => person.id)
+      .catch(() =>
+        alert(
+          'No actor named ' +
+            actor1Name +
+            ' was found. Please check the spelling and try again.'
+        )
+      );
+
+    const actor2ID = await axios(actor2Config)
+      .then((res) => res.data.results[0])
+      .then((person) => person.id)
+      .catch(() =>
+        alert(
+          'Cannot find an actor named ' +
+            actor2Name +
+            '. Please check the spelling and try again.'
+        )
+      );
+
+    getCredits(actor1ID, actor2ID);
+  };
+
+  const getCredits = async (actor1ID, actor2ID) => {
+    const getAxiosConfig = (actorID) => {
+      return {
+        method: 'GET',
+        url: `${baseUrl}/person/${actorID}/movie_credits`,
+        dataResponse: 'JSON',
+        params: {
+          api_key: apiKey,
+        },
+      };
+    };
+
+    const actor1Config = getAxiosConfig(actor1ID);
+
+    const actor2Config = getAxiosConfig(actor2ID);
+
+    const actor1Credits = await axios(actor1Config).then((res) => {
+      const credits = res.data.cast;
+      const fictionCredits = [];
+
+      for (let movie of credits) {
+        if (!movie.genre_ids.includes(99)) {
+          fictionCredits.push(movie.id);
+        }
+      }
+      return fictionCredits;
+    });
+
+    const actor2Credits = await axios(actor2Config).then((res) => {
+      const credits = res.data.cast;
+      const fictionCredits = [];
+
+      for (let movie of credits) {
+        if (!movie.genre_ids.includes(99)) {
+          fictionCredits.push(movie.id);
+        }
+      }
+      return fictionCredits;
+    });
+
+    findMatch(actor1Credits, actor2Credits);
+  };
+
+  const findMatch = async (actor1Credits, actor2Credits) => {
+    actor1Credits.forEach((movieID) => {
+      if (actor2Credits.includes(movieID)) {
+        getMovieObj(movieID);
+      }
+    });
+  };
+
+  const commonMovieArray = [];
+
+  const getMovieObj = (movieID) => {
+    axios({
+      method: 'GET',
+      url: `${baseUrl}/movie/${movieID}`,
+      dataResponse: 'JSON',
+      params: {
+        api_key: apiKey,
+      },
+    }).then((res) => {
+      const movie = res.data;
+      // console.log(movie);
+      commonMovieArray.push({
+        year: movie.release_date.slice(0, 4),
+        title: movie.title,
+        poster: movie.poster_path,
+        backgroundImage: movie.backdrop_path,
+        imdb: movie.imdb_id,
+        overview: movie.overview,
+      });
+    });
+  };
+
+  getActorIDs(actor1, actor2);
+  return (
+    <>
+      <p>{actor1}</p>
+      <p>{actor2}</p>
+    </>
+  );
+};
+
+export default Movies;
